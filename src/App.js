351535d8d8e86d0ff59ac746b07e24a5469ecc8b
Bernardo Raposo <braposo@gmail.com>
2016-12-22 18:41:48 +0000

    Updates code to use mobx for state updates and refactors where necessary

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import { select as d3select, mouse as d3mouse } from 'd3-selection';
 import { scaleLinear } from 'd3-scale';
+import { extendObservable, transaction, computed } from 'mobx';
+import { observer } from 'mobx-react';
 
 import Pythagoras from './Pythagoras';
 
 class App extends Component {
-    svg = {
-        width: 1280,
-        height: 600
-    };
-    state = {
-        currentMax: 0,
-        baseW: 80,
-        heightFactor: 0,
-        lean: 0
-    };
-    running = false;
-    realMax = 11;
+    constructor() {
+        super();
+        extendObservable(this, {
+            currentMax: 0,
+            x: 0,
+            y: 0,
+            heightFactor: computed(() => this.scaleFactor(this.y)),
+            lean: computed(() => this.scaleLean(this.x)),
+        });
+
+        this.baseW = 80;
+        this.svg = {
+            width: 1280,
+            height: 600
+        };
+        this.realMax = 11;
+        this.scaleFactor = scaleLinear()
+            .domain([this.svg.height, 0])
+            .range([0, .8]);
+
+        this.scaleLean = scaleLinear()
+            .domain([0, this.svg.width/2, this.svg.width])
+            .range([.5, 0, -.5]);
+    }
 
     componentDidMount() {
         d3select(this.refs.svg).on("mousemove", this.onMouseMove.bind(this));
@@ -27,34 +41,24 @@ class App extends Component {
     }
 
     next() {
-        const { currentMax } = this.state;
-
-        if (currentMax < this.realMax) {
-            this.setState({currentMax: currentMax + 1});
+        if (this.currentMax < this.realMax) {
+            this.currentMax += 1;
             setTimeout(this.next.bind(this), 500);
         }
     }
 
-    // Throttling approach borrowed from Vue fork
-    // https://github.com/yyx990803/vue-fractal/blob/master/src/App.vue
-    // rAF makes it slower than just throttling on React update
+    update(x, y) {
+        transaction(() => {
+            this.x = x;
+            this.y = y;
+        })
+    }
+
+    // Throttling doesn't seem to have much impact when using mobx
+    // so removed from here, but might be just the testing setup
     onMouseMove(event) {
-        if (this.running) return;
-        this.running = true;
-
-        const [x, y] = d3mouse(this.refs.svg),
-
-              scaleFactor = scaleLinear().domain([this.svg.height, 0])
-                                         .range([0, .8]),
-
-              scaleLean = scaleLinear().domain([0, this.svg.width/2, this.svg.width])
-                                       .range([.5, 0, -.5]);
-
-        this.setState({
-            heightFactor: scaleFactor(y),
-            lean: scaleLean(x)
-        });
-        this.running = false;
+        const [x, y] = d3mouse(this.refs.svg);
+        this.update(x,y);
     }
 
     render() {
@@ -68,14 +72,14 @@ class App extends Component {
                     <svg width={this.svg.width} height={this.svg.height} ref="svg"
                          style={{border: "1px solid lightgray"}}>
 
-                        <Pythagoras w={this.state.baseW}
-                                    h={this.state.baseW}
-                                    heightFactor={this.state.heightFactor}
-                                    lean={this.state.lean}
+                        <Pythagoras w={this.baseW}
+                                    h={this.baseW}
+                                    heightFactor={this.heightFactor}
+                                    lean={this.lean}
                                     x={this.svg.width/2-40}
-                                    y={this.svg.height-this.state.baseW}
+                                    y={this.svg.height-this.baseW}
                                     lvl={0}
-                                    maxlvl={this.state.currentMax}/>
+                                    maxlvl={this.currentMax}/>
 
                     </svg>
                 </p>
@@ -84,4 +88,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default observer(App);
